883c910ba0ad917be5cdc9377eb2b57911942df3
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2015-01-03 04:59:28 -0800

    plugins/cpu: add suite of cpu plugins

---
+++ plugins/cpu-darwin.c
@@ -0,0 +1,24 @@
+#include <mach/mach_host.h>
+#include <stdio.h>
+#include <err.h>
+
+int main(int argc, char *argv[])
+{
+  mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
+  mach_port_t nolo_port;
+  kern_return_t kr;
+  host_cpu_load_info_data_t cpuStats;
+
+  nolo_port = mach_host_self();
+  kr = host_statistics(nolo_port, HOST_CPU_LOAD_INFO, (host_info_t)&cpuStats, &count);
+  if (kr != KERN_SUCCESS) {
+    err(-1, "error from host_statistics(): %d", kr);
+  }
+
+  printf("user %u\n", cpuStats.cpu_ticks[CPU_STATE_USER]);
+  printf("idle %u\n", cpuStats.cpu_ticks[CPU_STATE_IDLE]);
+  printf("sys %u\n", cpuStats.cpu_ticks[CPU_STATE_SYSTEM]);
+  printf("nice %u\n", cpuStats.cpu_ticks[CPU_STATE_NICE]);
+
+  return 0;
+}
